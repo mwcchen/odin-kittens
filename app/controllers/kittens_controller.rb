3ad9c6f6828a6d1c3c5308b6d6662a76e7c0fa23
mwcchen <33730138+mwcchen@users.noreply.github.com>
2021-04-06 17:25:34 -0400

    Set up basic actions and views

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,51 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash.notice = "Kitten successfully created"
+      redirect_to @kitten
+    else
+      render :new
+    end
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      flash.notice = "Kitten successfully updated"
+      redirect_to @kitten
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    flash.notice = "Kitten successfully deleted"
+    redirect_to :root
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+
+end
